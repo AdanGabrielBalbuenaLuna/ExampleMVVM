a5ca402f9e329ac4d56971ec66aab026e8c79fff
AdanGabrielBalbuenaLuna <agbalbuenaluna@gmail.com>
2022-12-06 22:59:27 -0600

    -Second Commit- Retrofit Added MODEL/NETWORK/QuoteApiClient & QuoteService && MODEL/QuoteRepository Added Use case GetQuoteUseCase and GetRandomQuoteUseCase added -----Structure----- UI/view & viewmodel DOMAIN/usecases DATA/model/network CORE/RetrofitHelper

---
+++ app/src/main/java/com/example/examplemvvm/ui/viewmodel/QuoteViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.examplemvvm.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.examplemvvm.data.model.QuoteModel
+import com.example.examplemvvm.data.model.QuoteProvider
+import com.example.examplemvvm.domain.GetQuoteUseCase
+import com.example.examplemvvm.domain.GetRandomQuoteUseCase
+import kotlinx.coroutines.launch
+
+class QuoteViewModel: ViewModel() {
+    val quoteModel = MutableLiveData<QuoteModel>()
+    val isLoading = MutableLiveData<Boolean>()
+
+    var getQuoteUseCase = GetQuoteUseCase()
+    var getRandomQuoteUseCase = GetRandomQuoteUseCase()
+
+    fun onCreate() {
+        viewModelScope.launch {
+            isLoading.postValue(true)
+
+            val result = getQuoteUseCase()
+
+            if (!result.isNullOrEmpty()){
+                quoteModel.postValue(result[0])
+                isLoading.postValue(false)
+            }
+        }
+    }
+
+    fun quoteAleatoria(){
+        isLoading.postValue(true)
+        val quote = getRandomQuoteUseCase()
+        if (quote!=null){
+            quoteModel.postValue(quote)
+        }
+
+        isLoading.postValue(false)
+
+    }
+}
